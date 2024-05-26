2cbb6aee738eecc7e6ea4149af37b5074b5da24e
Développeur backend <141308438+Styven-Manaja@users.noreply.github.com>
2024-05-26 14:12:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log('Server listen at port : ', PORT);
+    console.log('Server launched.');
 })
